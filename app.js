4c51440b95f426b8abec0e8ba75dc751a68435bb
AliyaDevNova <124171140+aliyabatoolalvi@users.noreply.github.com>
2024-01-05 11:15:48 +0500

    updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+// let count = 0;
+
+// const value = document.querySelector("#value");
+// const btns = document.querySelectorAll(".btn");
+
+// btns.forEach(function (btn) {
+//     // console.log(btn);
+//     btns.addEventListener("click",function (e){
+//         console.log(e.currentTarget);
+//     })
+// });
+
+
 let count = 0;
 
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
-
+console.log(btns);
 btns.forEach(function (btn) {
-    // console.log(btn);
-    btns.addEventListener("click",function (e){
-        console.log(e.currentTarget);
-    })
+    console.log(btn);
+    btn.addEventListener("click", function (e) {
+       const style = e.currentTarget.classList;
+       if(style.contains('decrease')){
+        count--;
+       }
+       else if(style.contains('increase')){
+        count++;
+       }
+       else if(style.contains('reset')){
+        count= 0;
+       }
+       value.textContent = count;
+    });
 });
-
