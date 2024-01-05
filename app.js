b7151e1615fffd945504762349bfd66f21c7a23e
AliyaDevNova <124171140+aliyabatoolalvi@users.noreply.github.com>
2024-01-05 10:48:27 +0500

    Added some css

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function(item) {
-    console.log(item);
-    btns.addEventListener("click",function(e){
+btns.forEach(function (btn) {
+    // console.log(btn);
+    btns.addEventListener("click",function (e){
         console.log(e.currentTarget);
     })
 });
+
